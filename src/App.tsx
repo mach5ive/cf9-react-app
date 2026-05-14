17a234aa9d5c1ddfbd0b204ad90c43bf52edfde0
mach <manosxr@gmail.com>
2026-05-14 11:37:38 +0300

    feat(ui): implement ui on layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 // import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
 // import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 import Layout from "./components/Layout.tsx";
 
   //   type PropsA = {
@@ -58,19 +58,19 @@ function App() {
       {/*<ArrowFunctionalComponent />*/}
 
       {/*LESSON THREE*/}
-      <ArrowFunctionalComponentWithProps
-          title="Heading1"/>
-      <ArrowFunctionalComponentWithProps
-          title="Second Title"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aut dolor facilis fugit necessitatibus, nulla provident suscipit voluptatem voluptates voluptatum."
-      />
+      {/*<ArrowFunctionalComponentWithProps*/}
+      {/*    title="Heading1"/>*/}
+      {/*<ArrowFunctionalComponentWithProps*/}
+      {/*    title="Second Title"*/}
+      {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aut dolor facilis fugit necessitatibus, nulla provident suscipit voluptatem voluptates voluptatum."*/}
+      {/*/>*/}
 
-      <Layout>
-          <ArrowFunctionalComponentWithProps
-              title="Second Title"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aut dolor facilis fugit necessitatibus, nulla provident suscipit voluptatem voluptates voluptatum."
-          />
-          <p className='text-red-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque commodi cupiditate dolorem doloremque dolorum eius harum ipsa libero maxime minima molestiae odio odit officiis placeat porro possimus quasi quis quisquam quo quos, rem repellendus sapiente veritatis voluptatem. Asperiores autem distinctio dolorum eaque? Ad aliquid architecto corporis distinctio earum expedita iure laudantium non qui quibusdam reiciendis, rem repellendus repudiandae sed sequi soluta unde. Animi consectetur consequuntur cupiditate delectus deleniti distinctio doloremque dolores eius eveniet ex facilis fuga fugiat fugit in laudantium magnam natus nesciunt non nostrum praesentium quaerat quod, rem repellendus tempore velit voluptas voluptates. A deleniti dolores illum nam!</p>
+      <Layout addClasses="text-blue-600 bg-gray-100">
+          {/*<ArrowFunctionalComponentWithProps*/}
+          {/*    title="Second Title"*/}
+          {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aut dolor facilis fugit necessitatibus, nulla provident suscipit voluptatem voluptates voluptatum."*/}
+          {/*/>*/}
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque commodi cupiditate dolorem doloremque dolorum eius harum ipsa libero maxime minima molestiae odio odit officiis placeat porro possimus quasi quis quisquam quo quos, rem repellendus sapiente veritatis voluptatem. Asperiores autem distinctio dolorum eaque? Ad aliquid architecto corporis distinctio earum expedita iure laudantium non qui quibusdam reiciendis, rem repellendus repudiandae sed sequi soluta unde. Animi consectetur consequuntur cupiditate delectus deleniti distinctio doloremque dolores eius eveniet ex facilis fuga fugiat fugit in laudantium magnam natus nesciunt non nostrum praesentium quaerat quod, rem repellendus tempore velit voluptas voluptates. A deleniti dolores illum nam!</p>
       </Layout>
     </>
   )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
         <>
-            <footer>
-                This is a footer area!
+            <footer className="bg-cf-dark-gray text-white">
+                <div className="container mx-auto py-8 text-center">
+                    &copy; {currentYear} Coding Factory 9. All rights reserved.
+                </div>
             </footer>
         </>
     )
